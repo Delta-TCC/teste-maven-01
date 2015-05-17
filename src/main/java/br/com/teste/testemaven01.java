6563a2d7def4c97e5e60a4f11dd737da80cd6b3f
alexfdealmeida <alexferreiradealmeida@gmail.com>
2015-05-17 18:32:02 -0300

    Adição dos fontes.

---
+++ src/main/java/br/com/teste/testemaven01.java
@@ -0,0 +1,13 @@
+package br.com.teste;
+
+import org.apache.commons.lang3.StringUtils;
+
+public class testemaven01 {
+
+    public static void main(String[] args) {
+
+        System.out.println(StringUtils.capitalize("Alex"));
+
+    }
+
+}
